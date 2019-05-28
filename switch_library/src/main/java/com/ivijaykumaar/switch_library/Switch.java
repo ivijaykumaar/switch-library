c680eae1574f5338ae9868f4ca126d0e3937ac83
vijay-off <vijay.v@appraamlabs.com>
2019-05-28 12:55:29 +0530

    first commit

---
+++ switch_library/src/main/java/com/ivijaykumaar/switch_library/Switch.java
@@ -0,0 +1,13 @@
+package com.ivijaykumaar.switch_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Switch {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
